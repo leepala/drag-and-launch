3863826c3b102e562dd5698ed4622aa441bde85b
Rhonin Lee <rhoninl@hotmail.com>
2023-12-02 20:13:06 +0800

    refactor

--- Launch/GameScene.swift
+++ Launch/GameScene.swift
@@ -6,83 +6,146 @@
 //
 
 import SpriteKit
-import GameplayKit
 
-class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var scoreLabel: SKLabelNode!
+    var block: SKShapeNode?
+    var goal: SKSpriteNode?
+    let slingShotCenter = CGPoint(x: 0, y: -300)
+    let goalPostion = CGPoint(x: 0, y: 300)
+    let maxSlingDistance: CGFloat = 150.0
+    let slingshotRadius: CGFloat = 3
+    var score: Int = 0
+    var isDraggingBlock = false
     
     override func didMove(to view: SKView) {
+        self.physicsWorld.contactDelegate = self
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        scoreLabel = SKLabelNode(fontNamed: "Arial")
+        scoreLabel.position = CGPoint(x: 0, y: 350)
+        scoreLabel.fontSize = 100
+        scoreLabel.text = "Drag!"
+        addChild(scoreLabel)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        let slingshot = SKShapeNode(circleOfRadius: slingshotRadius)
+        slingshot.position = slingShotCenter
+        slingshot.fillColor = .gray
+        addChild(slingshot)
+        
+        createGoal(at: goalPostion)
+        createBlock(at: slingShotCenter)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        guard let touch = touches.first else { return }
+        guard block != nil else { return }
+        let location = touch.location(in: self)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        if block!.frame.contains(location) {
+            isDraggingBlock = true
+            block!.physicsBody!.isDynamic = false
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        guard let touch = touches.first, isDraggingBlock else { return }
+        var location = touch.location(in: self)
+        let offset = CGPoint(x: location.x - slingShotCenter.x, y: location.y - slingShotCenter.y)
+        let distance = sqrt(offset.x * offset.x + offset.y * offset.y)
+        if location.y > slingShotCenter.y {
+            location.x = slingShotCenter.x + (offset.x / distance) * maxSlingDistance
+            location.y = slingShotCenter.y
+        } else if distance > maxSlingDistance {
+            location.y = slingShotCenter.y + (offset.y / distance) * maxSlingDistance
+            location.x = slingShotCenter.x + (offset.x / distance) * maxSlingDistance
+        }
+        
+        block!.position = location
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        if isDraggingBlock {
+            let dx = slingShotCenter.x - block!.position.x
+            let dy = slingShotCenter.y - block!.position.y
+            let launchVelocity = CGVector(dx: dx * 1.2, dy: dy * 1.1)
+            block!.physicsBody!.isDynamic = true
+            block!.physicsBody!.applyImpulse(launchVelocity)
+            isDraggingBlock = false
+        }
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        if let block = block, !frame.intersects(block.frame) {
+            block.removeFromParent()
+            self.block = nil
+             
+            let ssc = slingShotCenter
+            let delayAction = SKAction.wait(forDuration: 0.2)
+            let createAction = SKAction.run { [weak self] in
+                self?.createBlock(at: ssc)
+                self?.createGoal(at: nil)
+            }
+            self.run(SKAction.sequence([delayAction, createAction]))
+        }
     }
+
+    func didBegin(_ contact: SKPhysicsContact) {
+        let mask = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
+
+        if mask == (PhysicsCategories.block | PhysicsCategories.goal) {
+            score += 1
+            
+            scoreLabel.text = "\(score)"
+            self.goal?.removeFromParent()
+            self.goal = nil
+        }
+    }
+
+    
+    func createBlock(at position: CGPoint) {
+        block = SKShapeNode(circleOfRadius: 20)
+        block!.position = position
+        block!.fillColor = .red
+        block!.physicsBody = SKPhysicsBody(circleOfRadius: 20)
+        block!.physicsBody?.categoryBitMask = PhysicsCategories.block
+        block!.physicsBody?.contactTestBitMask = PhysicsCategories.goal
+        block!.physicsBody?.collisionBitMask = 0
+        block!.physicsBody!.isDynamic = false
+        self.addChild(block!)
+    }
+
+    func createGoal(at position: CGPoint?) {
+        guard goal == nil else { return }
+        
+        var targetPostion = position
+        if targetPostion == nil {
+            targetPostion = RandLocation()
+        }
+        
+        goal = SKSpriteNode(color: UIColor(red: 0.4, green: 0.4, blue: 0.9, alpha: 0.8), size: CGSize(width: 100, height:10))
+        
+        goal!.position = targetPostion!
+        goal!.physicsBody = SKPhysicsBody(rectangleOf: goal!.size)
+        goal!.physicsBody?.categoryBitMask = PhysicsCategories.goal
+        goal!.physicsBody?.collisionBitMask = 0
+        goal!.physicsBody?.contactTestBitMask = PhysicsCategories.block
+        goal!.physicsBody?.isDynamic = false
+        self.addChild(goal!)
+    }
+    
+    
+    func RandLocation() -> CGPoint {
+        let x: Int = Int.random(in: -250...250)
+        let y: Int = Int.random(in: 0...400)
+        
+        return CGPoint(x: x, y: y)
+    }
+}
+
+
+struct PhysicsCategories {
+    static let block: UInt32 = 0x1 << 1
+    static let goal: UInt32 = 0x1 << 2
 }
